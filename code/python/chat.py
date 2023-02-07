9914eb43ff4ad6c6a15bea9ed65428371988b042
kugitai <98938330+kugitai@users.noreply.github.com>
2023-02-07 21:30:15 +0900

    a

--- code/python/chat.py
+++
@@ -1,7 +0,0 @@
-name = input('名前を入力してください')
-while True:
-    chat = input()
-    if chat != 'quit':
-        print(name+':'+chat)
-    else:
-        break
--- code/python/passward.py
+++
@@ -1,9 +0,0 @@
-passward = 'asked'
-while True:
-    inpass = input('パスワードを入力してください')
-    if inpass != passward:
-        print("異なります")
-    else:
-        print("承認しました")
-        break
-        
